8f4bad13417a2c9f454101d92f39cee5b8999d87
Adonica Villa <villa2232782@mls.ceu.edu.ph>
2025-02-04 12:48:59 +0800

    Demo-03

--- demo-03.js
+++ demo-03.js
@@ -1,32 +1,15 @@
-// let numbers = ['12,7,5, 20, 33, 42, 19, 8, 10'];
-// let EvenNumber = [];
-// let OddNumber = [];
-
-// numbers.forEach(number=>{
-//     if(numbers % 2 === 0){
-//         EvenNumber.push(numbers);
-//     } else{
-//         OddNumber.push(numbers);
-//     }
-// });
-
-// console.log("Number:", numbers)
-// console .log("Even Number:", EvenNumber, "| Count", EvenNumber.length)
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let evenNumbers = [];
-let oddNumbers = [];
+let numbers = [12,7,5, 20, 33, 42, 19, 8, 10];
+let EvenNumber = [];
+let OddNumber = [];
 
 numbers.forEach(number => {
-    if (number % 2 === 0) {
-        evenNumbers.push(number);
-    } else {
-        oddNumbers.push(number);
+    if(number % 2 === 0){
+        EvenNumber.push(number);
+    } else{
+        OddNumber.push(number);
     }
 });
 
-
-// console.log("Number:", numbers)
-console.log('Even numbers:', evenNumbers, evenNumbers.length);
-console.log("Odd numbers:", oddNumbers);
-// console.log("Number:", numbers);
-// console .log("Even Number:", evenNumber, "| Count", evenNumber.length);
+console.log("Number:", numbers.join());
+console .log("Even Number:", EvenNumber.join(), "| Count", EvenNumber.length);
+console .log("Odd Number:", OddNumber.join(),"| Count", OddNumber.length);
