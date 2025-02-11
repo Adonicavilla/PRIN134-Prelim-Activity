068e927aa089115ffee6c55ea0784b7211291820
Adonicavilla <124745683+Adonicavilla@users.noreply.github.com>
2025-02-11 21:11:19 +0800

    Update demo-05.js

--- demo-05.js
+++ demo-05.js
@@ -1,45 +1,17 @@
-
-let grades1 = [80, 90, 77, 82, 68, 98, 89, 79, 41, 80];
-let grades2 = [170, 68, 77, 82, 68, 65, 80, 75, 93, 78];
-let grades3 = [56, 50, 45, 98, 75, 50, 68, 34, 91, 19];
-let grades4 = [10, 30, 70, 31, 90, 20, 99, 58, 67, 21];
-
-
-function calculateAverage(grades) {
-    let total = grades.reduce((sum, grade) => sum + grade, 0);
-    return total / grades.length;
+function getGrades(grades) {
+    let avg = grades.reduce((a, b) => a + b) / grades.length;
+    
+    let rating = 'F';
+    if (avg >= 90) rating = 'A';
+    else if (avg >= 80) rating = 'B';
+    else if (avg >= 70) rating = 'C';
+    else if (avg >= 60) rating = 'D';
+    
+    console.log(`Grades: [${grades}]. Grade average: ${avg.toFixed(1)}. Your rating: ${rating}. You ${rating === 'F' ? 'failed' : 'passed'}.`);
 }
 
-function getRating(average) {
-    if (average >= 90) {
-        return 'A';
-    } else if (average >= 80) {
-        return 'B';
-    } else if (average >= 70) {
-        return 'C';
-    } else if (average >= 60) {
-        return 'D';
-    } else {
-        return 'F';
-    }
-}
-
-
-function getPassStatus(rating) {
-    return rating !== 'F' ? 'You passed.' : 'You failed.';
-}
-
-function printResults(grades) {
-    let averageGrade = calculateAverage(grades);
-    let rating = getRating(averageGrade);
-    let passStatus = getPassStatus(rating);
-
-    console.log('Grades:', grades.join(', '));
-    console.log('Grade average:', averageGrade.toFixed(1));
-    console.log('Your rating:', rating);
-    console.log(passStatus);
-    console.log('--------------------------------------');
-}
-
-
-console.log(printResults);
+getGrades([85, 93, 89, 89, 85, 92, 100, 90, 80, 100]);
+getGrades([80, 90, 77, 82, 68, 98, 89, 79, 93, 100]);
+getGrades([70, 68, 77, 82, 68, 65, 89, 79, 93, 70]);
+getGrades([56, 50, 89, 90, 75, 68, 34, 91, 19, 67]);
+getGrades([10, 30, 70, 31, 90, 20, 89, 58, 67, 21]);
